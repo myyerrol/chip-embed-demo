dc48245c2f3a86fa9d5cdf441795648d4024cd87
myyerrol <myyerrol@126.com>
2025-06-01 12:00:00 +0800

    feat: add dht22 for main

--- wch/temp-humi-clock/User/main.c
+++ wch/temp-humi-clock/User/main.c
@@ -22,83 +22,108 @@
  *
  */
 
-#include "debug.h"
+// #include "debug.h"
 
 
-/* Global define */
+// /* Global define */
 
 
-/* Global Variable */
-vu8 val;
+// /* Global Variable */
+// vu8 val;
 
-/*********************************************************************
- * @fn      USARTx_CFG
- *
- * @brief   Initializes the USART2 & USART3 peripheral.
- *
- * @return  none
- */
-void USARTx_CFG(void)
-{
-    GPIO_InitTypeDef  GPIO_InitStructure = {0};
-    USART_InitTypeDef USART_InitStructure = {0};
+// /*********************************************************************
+//  * @fn      USARTx_CFG
+//  *
+//  * @brief   Initializes the USART2 & USART3 peripheral.
+//  *
+//  * @return  none
+//  */
+// void USARTx_CFG(void)
+// {
+//     GPIO_InitTypeDef  GPIO_InitStructure = {0};
+//     USART_InitTypeDef USART_InitStructure = {0};
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
+//     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
 
-    /* USART1 TX-->D.5   RX-->D.6 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
+//     /* USART1 TX-->D.5   RX-->D.6 */
+//     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+//     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
+//     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+//     GPIO_Init(GPIOD, &GPIO_InitStructure);
+//     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+//     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+//     GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+//     USART_InitStructure.USART_BaudRate = 115200;
+//     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+//     USART_InitStructure.USART_StopBits = USART_StopBits_1;
+//     USART_InitStructure.USART_Parity = USART_Parity_No;
+//     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+//     USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
 
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
-}
+//     USART_Init(USART1, &USART_InitStructure);
+//     USART_Cmd(USART1, ENABLE);
+// }
 
-/*********************************************************************
- * @fn      main
- *
- * @brief   Main program.
- *
- * @return  none
- */
-int main(void)
-{
+// /*********************************************************************
+//  * @fn      main
+//  *
+//  * @brief   Main program.
+//  *
+//  * @return  none
+//  */
+// int main(void)
+// {
+//     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+//     SystemCoreClockUpdate();
+//     Delay_Init();
+// #if (SDI_PRINT == SDI_PR_OPEN)
+//     SDI_Printf_Enable();
+// #else
+//     USART_Printf_Init(115200);
+// #endif
+//     printf("SystemClk:%d\r\n",SystemCoreClock);
+//     printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+
+//     USARTx_CFG();
+
+//     while(1)
+//     {
+
+//         while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
+//         {
+//             /* waiting for receiving finish */
+//         }
+//         val = (USART_ReceiveData(USART1));
+//         USART_SendData(USART1, ~val);
+//         while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
+//         {
+//             /* waiting for sending finish */
+//         }
+//     }
+// }
+
+
+
+
+
+
+#include <dht22.h>
+
+int main(void) {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
     SystemCoreClockUpdate();
-    Delay_Init();
-#if (SDI_PRINT == SDI_PR_OPEN)
-    SDI_Printf_Enable();
-#else
     USART_Printf_Init(115200);
-#endif
-    printf("SystemClk:%d\r\n",SystemCoreClock);
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+    
+    Delay_Ms(2000);
 
-    USARTx_CFG();
-
-    while(1)
-    {
-
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
-        {
-            /* waiting for receiving finish */
+    uint8_t DHT22_Buf[5];
+    while (1) {
+        if (DHT22_Get_Temp_Humi_Data(DHT22_Buf)) {
+            printf("Temp: %d.%d Humi: %d.%d\n", DHT22_Buf[2], DHT22_Buf[3], DHT22_Buf[0], DHT22_Buf[1]);
         }
-        val = (USART_ReceiveData(USART1));
-        USART_SendData(USART1, ~val);
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        {
-            /* waiting for sending finish */
-        }
+        Delay_Ms(2000);
     }
+
+    return 0;
 }
